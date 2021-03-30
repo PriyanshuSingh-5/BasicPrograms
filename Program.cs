4808f355ee52feb1ac18b3e39725e230d7e78cda
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-03-30 21:34:58 +0530

    [priyanshu]added

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Sample2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ReplaceString.ReadInput();
+            Console.Read();
+        }
+    }
+}
---
+++ ReplaceString.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sample2
+{
+    class ReplaceString
+    }
+        public static void ReadInput
+        { 
+    string template = "Hello <<Username>> ,How are you?";
+    Console.WriteLine("please enter your name");
+            string UserInput = Console.ReadLine();
+    ReplaceWord(template, UserInput);
+}       
+
+
+    private void ReplaceWord(string template,string UserInput)
+    
+    {
+        string result = template.Replace("<<username>>", UserInput);
+        Console.WriteLine("after done replace" + result);
+    }
+}
