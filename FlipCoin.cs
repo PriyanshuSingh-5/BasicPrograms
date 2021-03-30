491f9740d597f99538dd4fc9a789116f454b7a23
PriyanshuSingh-5 <singhprin05@gmail.com>
2021-03-31 03:55:38 +0530

    [priyanshu]modified1

---
+++ FlipCoin.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+public class Flipcoin
+{
+	public static void ReadInput()
+	{
+		Console.WriteLine("please enter no. of times to flip coin");
+		int numOfTimes = Convert.ToInt32(Console.ReadLine());
+		Flipcoin(numOfTimes);
+	}
+	private static vid Flip(int n)
+    {
+		int tail = 0;
+		int head = 0;
+		Random random = new Random();
+		for(int i=1;i<=n;i++)
+        {
+			if (random.NextDouble() < 0.5)
+				tail++
+					else
+				head++;
+        }
+    }
+}
+double tailPercentage = (double)tailPercentage / numOfTimes * 100;
+double headPercentage = (double)headPercentage / numOfTimes * 100;
+Console.WriteLine("tailcount" + "=" + tailPercentage + "\tPercentage");
+Console.WriteLine("headcount" + "=" + headPercentage + "\tPercentage");
+
+
